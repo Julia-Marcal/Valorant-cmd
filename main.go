42fb1b7b2ee6973f4e4a599a2f0b9b7022d93ddf
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-09-26 18:45:59 -0300

    returning data about latest matches, best characthers etc...

--- main.go
+++ main.go
@@ -25,9 +25,16 @@ func main() {
 		nameValue := m.Inputs[cmd.Name].Value()
 		tagValue := m.Inputs[cmd.Tag].Value()
 
-		reg, lvl, img, err_fetch := val.FetchAccount(nameValue, tagValue)
+		reg, puuid, lvl, img, err_fetch := val.FetchAccount(nameValue, tagValue)
 
 		if err_fetch != nil {
+			fmt.Println("Oh no:", err_fetch)
+			os.Exit(1)
+		}
+
+		matchInfo, mapINfo, avarages, bestCharacter, matchErr := val.FetchMatches(reg, puuid)
+
+		if matchErr != nil {
 			fmt.Println("Oh no:", err_fetch)
 			os.Exit(1)
 		}
@@ -35,6 +42,20 @@ func main() {
 		fmt.Printf("Account region: %s\n", reg)
 		fmt.Printf("Account level: %d\n", lvl)
 		fmt.Printf("Account card: %s\n", img)
+
+		fmt.Printf("\n")
+
+		fmt.Printf("Last map played: %s\n", mapINfo["Name"])
+		fmt.Printf("Last match kill: %d\n", matchInfo[0])
+		fmt.Printf("Last match deaths: %d\n", matchInfo[1])
+		fmt.Printf("Last match assists: %d\n", matchInfo[2])
+
+		fmt.Printf("\n")
+
+		fmt.Printf("Average kills from last matches %.0f\n", avarages[0])
+		fmt.Printf("Average death from last matches %.0f\n", avarages[1])
+		fmt.Printf("Average assists from last matches %.0f\n", avarages[2])
+		fmt.Printf("Your best character is: %s\n", bestCharacter)
 	}
 
 }
